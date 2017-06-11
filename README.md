2034742a657193f71f68c406dc6dd066648ad7ea
jgaye <jgaye@point.io>
2017-06-11 16:04:02 +0200

    Example unable from IntelliJ, command line, or packaged jar

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ Experimenting with Spring Boot
 
 ## Learning curve
 * Created maven project with IntelliJ and added spring dependency - 1h
+* Using [the spring doc](https://docs.spring.io/spring-boot/docs/current-SNAPSHOT/reference/htmlsingle/#getting-started-first-application-code) to create a hello world example, run it with IntelliJ (debug is available), and creating an executable Jar - 25mins
---
+++ src/main/java/Example.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Marvin on 11/06/2017.
+ */
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@EnableAutoConfiguration
+public class Example {
+
+    @RequestMapping("/")
+    String home() {
+        return "<b>Hello</b> World!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Example.class, args);
+    }
+
+}
